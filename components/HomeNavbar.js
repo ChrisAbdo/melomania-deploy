732397c6852155b81152fa2bba161c68931e6274
ChrisAbdo <chris.j.abdo@gmail.com>
2022-07-04 05:25:16 -0400

    ADDED: lots of work to landing page

--- components/HomeNavbar.js
+++ components/HomeNavbar.js
@@ -1,7 +1,17 @@
 import Link from "next/link";
 import { useState } from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
-import { Button, Menu, MenuButton, MenuList, MenuItem } from "@chakra-ui/react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  InputGroup,
+  InputLeftElement,
+  Input,
+  SearchIcon,
+} from "@chakra-ui/react";
 import { ethers } from "ethers";
 import { ChevronDownIcon } from "@chakra-ui/icons";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -81,63 +91,17 @@ function Navbar() {
         <h1 className={styles.navbarText}>melomania</h1>
       </Link>
 
-      <form class="flex items-center">
-        <label for="simple-search" class="sr-only">
-          Search
-        </label>
-        <div class="relative w-full">
-          <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-            <svg
-              class="w-5 h-5 text-gray-500 dark:text-gray-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clip-rule="evenodd"
-              ></path>
-            </svg>
-          </div>
-          <input
-            type="text"
-            id="simple-search"
-            class="bg-gray-50 border border-gray-500  text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Search"
-            required
-          />
-        </div>
-        <button
-          type="submit"
-          class="p-2.5 ml-2 text-sm font-medium  rounded-lg border border-gray-500"
-        >
-          <svg
-            class="w-5 h-5"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
-        </button>
-      </form>
+      <form class="flex items-center"></form>
 
       <div className={styles.navbarHeader}>
         <Link href="/">
-          <Button variant="ghost">Home</Button>
+          <Button variant="ghost">home</Button>
         </Link>
         <Link href="/marketplace">
-          <Button variant="ghost">Marketplace</Button>
+          <Button variant="ghost">marketplace</Button>
         </Link>
         <Link href="/profile">
-          <Button variant="ghost">Profile</Button>
+          <Button variant="ghost">profile</Button>
         </Link>
       </div>
       <div className="flex items-center">
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,11 @@
 import Head from "next/head";
-import Link from "next/link";
 import Navbar from "../components/HomeNavbar";
-import { Text, Button } from "@chakra-ui/react";
-import { Component, useState } from "react";
-import styles from "../styles/Home.module.css";
-import { ConnectButton } from "@rainbow-me/rainbowkit";
-import Web3 from "web3";
-import Melomania from "../build/contracts/Melomania.json";
-import { ethers } from "ethers";
+import styles from "../styles/index.module.css";
+import { GiMusicalNotes } from "react-icons/gi";
+import { BiStore } from "react-icons/bi";
+import { SiEthereum } from "react-icons/si";
 
 function Home() {
-  const [walletAddress, setWalletAddress] = useState("");
-
   return (
     <div>
       <Head>
@@ -23,7 +17,7 @@ function Home() {
         />
       </Head>
 
-      <main>
+      {/* <main>
         <div className={styles.welcomeText}>
           <div>
             <Navbar />
@@ -51,7 +45,6 @@ function Home() {
               </Text>
               <div className="mt-6 ">
                 <div className="flex flex-col items-center justify-center mb-6">
-                  {/* if user is logged in, make button id='revealMe' not hidden*/}
 
                   <ConnectButton />
 
@@ -65,7 +58,190 @@ function Home() {
             </div>
           </div>
         </div>
-      </main>
+      </main> */}
+      <Navbar />
+      <div className={styles.headerText}>
+        <section
+          class="h-screen
+      flex flex-col items-center justify-center"
+        >
+          <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-lg mx-auto text-center">
+              <h2 class="text-3xl font-bold sm:text-4xl">melomania🎶</h2>
+
+              <p class="mt-4 ">
+                decentralized music marketplace and crowdsourcing platform
+              </p>
+            </div>
+
+            <div class="grid grid-cols-1 gap-8 mt-8 md:grid-cols-2 lg:grid-cols-3">
+              <div
+                class="cursor-pointer block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <GiMusicalNotes className="h-7 w-7" />
+
+                <h3 class="mt-4  text-xl font-bold border-b border-gray-500 ">
+                  explore new music
+                </h3>
+
+                <p class="mt-1 text-sm  ">
+                  with no genre filter, melomania encourages you to explore new
+                  music through community favorites. you never know, you might
+                  find something you like ;)
+                </p>
+              </div>
+
+              <div
+                class="cursor-pointer block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <BiStore className="h-7 w-7" />
+
+                <h3 class="mt-4  text-xl font-bold border-b border-gray-500 ">
+                  marketplace
+                </h3>
+
+                <p class="mt-1 text-sm ">
+                  to view and like/tip songs, head over to the marketplace. this
+                  is where you'll be able to upload your own songs as well
+                </p>
+              </div>
+
+              <div
+                class="cursor-pointer block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <SiEthereum className="h-7 w-7" />
+
+                <h3 class="mt-4  text-xl font-bold border-b border-gray-500 ">
+                  crowdsourcing
+                </h3>
+
+                <p class="mt-1 text-sm ">
+                  we all know its hard being an artist. but the community can
+                  help. post a couple of your songs and potentially get the
+                  funding for your next hit album.
+                </p>
+              </div>
+
+              <a
+                class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-10 h-10 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  />
+                </svg>
+
+                <h3 class="mt-4 text-xl font-bold ">Digital campaigns</h3>
+
+                <p class="mt-1 text-sm ">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                  quo possimus adipisci distinctio alias voluptatum blanditiis
+                  laudantium.
+                </p>
+              </a>
+
+              <a
+                class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-10 h-10 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  />
+                </svg>
+
+                <h3 class="mt-4 text-xl font-bold ">Digital campaigns</h3>
+
+                <p class="mt-1 text-sm ">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                  quo possimus adipisci distinctio alias voluptatum blanditiis
+                  laudantium.
+                </p>
+              </a>
+
+              <a
+                class="block p-8 transition border border-gray-800 shadow-xl rounded-xl hover:shadow-gray-500/10 hover:border-gray-500/10"
+                href="/services/digital-campaigns"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  class="w-10 h-10 "
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z" />
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z" />
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  />
+                </svg>
+
+                <h3 class="mt-4 text-xl font-bold ">Digital campaigns</h3>
+
+                <p class="mt-1 text-sm ">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ut
+                  quo possimus adipisci distinctio alias voluptatum blanditiis
+                  laudantium.
+                </p>
+              </a>
+            </div>
+
+            <div class="mt-12 text-center">
+              <a
+                class="inline-flex items-center px-8 py-3 mt-8   border border-pink-600 rounded hover:bg-transparent active:text-pink-500 focus:outline-none focus:ring"
+                href="/get-started"
+              >
+                <span class="text-sm font-medium"> Get Started </span>
+
+                <svg
+                  class="w-5 h-5 ml-3"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M17 8l4 4m0 0l-4 4m4-4H3"
+                  />
+                </svg>
+              </a>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
